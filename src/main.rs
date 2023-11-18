4654e19bdb5802de714e455bee8423905adac35d
root309 <choco00101x@gmail.com>
2023-11-19 06:04:12 +0900

    testcase

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 mod lexer;
 mod parser;
 mod ast;
+mod interpreter;
+
+use std::fs;
 use lexer::Lexer;
 use parser::Parser;
+use interpreter::Interpreter;
 
 fn main() {
-    let input = "let x = 5 + 10;";
-    let mut lexer = Lexer::new(input);
-    match lexer.lex() {
-        Ok(tokens) => {
-            let mut parser = Parser::new(tokens);
-            match parser.parse_expression() {
-                Ok(ast) => println!("AST: {:?}", ast),
-                Err(e) => eprintln!("Parser error: {:?}", e),
-            }
+    let filename = "program.txt";
+    let input = fs::read_to_string(filename)
+        .expect("Failed to read file");
+    let mut lexer = Lexer::new(&input);
+    let tokens = match lexer.lex() {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("Lexer error: {:?}", e);
+            return;
         }
-        Err(e) => eprintln!("Lexer error: {:?}", e),
-    }
+    };
+
+    let mut parser = Parser::new(tokens);
+    let ast = match parser.parse_expression() {
+        Ok(ast) => ast,
+        Err(e) => {
+            eprintln!("Parser error: {:?}", e);
+            return;
+        }
+    };
+
+    let interpreter = Interpreter::new();
+    let result = interpreter.interpret(ast);
+    println!("Result: {}", result);
 }
